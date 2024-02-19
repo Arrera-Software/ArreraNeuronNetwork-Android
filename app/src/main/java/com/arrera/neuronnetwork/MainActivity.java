ae6da6ace3db3c4d285faebbbb9358d33f5ab22a
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-02-19 11:29:25 +0100

    Programme principal

--- app/src/main/java/com/arrera/neuronnetwork/MainActivity.java
+++ app/src/main/java/com/arrera/neuronnetwork/MainActivity.java
@@ -3,6 +3,7 @@ package com.arrera.neuronnetwork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CArreraNeuronNetwork arreraNetwork = new CArreraNeuronNetwork("Opale","Developper un algo d'assistant pour android",true,"monsieur","dev") ;
+        // Recuperation des widget
+        TextView textSortie = findViewById(R.id.IDC_SORTIE);
 
+        //Programme de test
+        textSortie.setText(arreraNetwork.salutation());
     }
 }
