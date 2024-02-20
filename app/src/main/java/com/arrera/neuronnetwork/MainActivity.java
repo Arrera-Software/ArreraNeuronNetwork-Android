5b172baf5dd5dd35a9394d16f788c17e67e2b4b6
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-02-20 11:35:59 +0100

    Fonction aurevoir

--- app/src/main/java/com/arrera/neuronnetwork/MainActivity.java
+++ app/src/main/java/com/arrera/neuronnetwork/MainActivity.java
@@ -3,6 +3,8 @@ package com.arrera.neuronnetwork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +16,18 @@ public class MainActivity extends AppCompatActivity {
         CArreraNeuronNetwork arreraNetwork = new CArreraNeuronNetwork("Opale","Developper un algo d'assistant pour android",true,"monsieur","dev") ;
         // Recuperation des widget
         TextView textSortie = findViewById(R.id.IDC_SORTIE);
+        Button btnQUIT = findViewById(R.id.IDC_QUIT);
 
         //Programme de test
+        //textSortie.setText(Integer.toString(new CArreraDate().getHour()));
         textSortie.setText(arreraNetwork.salutation());
+
+        btnQUIT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textSortie.setText(arreraNetwork.aurevoir());
+                //finish();
+            }
+        });
     }
 }
