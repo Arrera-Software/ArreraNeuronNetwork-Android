20e2ac27d5205036f4c6b74d550f55c4ee337e16
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-02-20 14:34:40 +0100

    Ajout de l'historique

--- app/src/main/java/com/arrera/neuronnetwork/CArreraNeuronNetwork.java
+++ app/src/main/java/com/arrera/neuronnetwork/CArreraNeuronNetwork.java
@@ -3,6 +3,8 @@ package com.arrera.neuronnetwork;
 
 
 public class CArreraNeuronNetwork {
+
+    private String oldRequette , oldSortie;
     private CArreraDate date;
     private CGestionnaireNeuron gestionnaireNeuron ;
     private CNeuronFormulation nFormulation;
@@ -16,15 +18,23 @@ public class CArreraNeuronNetwork {
 
     public String booting()
     {
-        return this.nFormulation.salutation();
+        String text = this.nFormulation.salutation();
+        this.oldRequette = "boot";
+        this.oldSortie =  text;
+        return text ;
     }
     public String shutdown(){
-        return this.nFormulation.aurevoir();
+        String text = this.nFormulation.aurevoir();
+        this.oldRequette = "shutdown";
+        this.oldSortie =  text;
+        return text ;
     }
 
     public String neuron(String requette){
         String sortie;
         sortie = this.nFormulation.nocomprehension();
+        this.oldRequette = requette;
+        this.oldSortie =  sortie;
         return sortie;
     }
 
