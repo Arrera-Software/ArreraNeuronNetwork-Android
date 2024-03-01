0394e88d92f6da4a8eaf05f85cf20960556f499b
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-03-01 15:50:06 +0100

    Declaration des bouton

--- app/src/main/java/com/arrera/neuron/MainActivity.kt
+++ app/src/main/java/com/arrera/neuron/MainActivity.kt
@@ -2,10 +2,21 @@ package com.arrera.neuron
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val msgOUT = findViewById<TextView>(R.id.IDC_OUTNEURON);
+        val msgIN = findViewById<TextView>(R.id.IDC_INMSG);
+        val btnSend = findViewById<Button>(R.id.IDC_SEND);
+
+        btnSend.setOnClickListener(){
+            val text = msgIN.text.toString();
+            
+        }
     }
 }
