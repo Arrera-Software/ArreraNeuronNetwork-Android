b98c288df6187232984bdb4155293b60c4e2b123
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-03-04 13:59:45 +0100

    Ajout fonction localisation

--- app/src/main/java/com/arrera/neuron/MainActivity.kt
+++ app/src/main/java/com/arrera/neuron/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.arrera.neuron
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import  android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity()  {
+class MainActivity : AppCompatActivity() , LocationListener  {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val locationManager : LocationManager
         setContentView(R.layout.activity_main)
         // Declaration widget
         val msgOUT = findViewById<TextView>(R.id.IDC_OUTNEURON);
@@ -18,6 +28,18 @@ class MainActivity : AppCompatActivity()  {
         val arreraNeuron = CArreraNetworkNeuron("Opale", "but", true, "Monsieur", "dev");
         // demarage
         msgOUT.setText("Opale : "+arreraNeuron.bonjour());
+        // Localisation
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED
+            ){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),100)
+        }
+        try {
+            val locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this@MainActivity)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
         btnSend.setOnClickListener() {
             val text = msgIN.text.toString();
             msgIN.setText("");
@@ -29,4 +51,9 @@ class MainActivity : AppCompatActivity()  {
 
         }
     }
+
+    override fun onLocationChanged(location: Location) {
+    }
+
+
 }
