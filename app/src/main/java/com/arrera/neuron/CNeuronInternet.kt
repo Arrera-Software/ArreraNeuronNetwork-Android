28dfad66fa190b4a262bda33ef1274ecb0cc65eb
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-03-18 10:54:32 +0100

    Ajout youtube et youtube music

--- app/src/main/java/com/arrera/neuron/CNeuronInternet.kt
+++ app/src/main/java/com/arrera/neuron/CNeuronInternet.kt
@@ -11,11 +11,37 @@ class CNeuronInternet(private val const: Context,
 
     fun neuron(requette:String)
     {
+        var etatVous =  gestionnaire.etatVous
+        var genre = gestionnaire.genre
+        var user = gestionnaire.user
         if(requette.contains("recherche"))
         {
             val url = "https://www.google.com/search?q="+requette.replace("recherche","")
             web.open(url);
             sortietext = "Voici votre recherche"
+        }
+        else
+        {
+            if (requette.contains("youtube"))
+            {
+                sortietext = "voici youtube"
+                web.open("https://www.youtube.com/")
+            }
+            else
+            {
+               if(requette.contains("musique")||requette.contains("yt music")||requette.contains("music"))
+               {
+                   if (etatVous)
+                   {
+                       sortietext = "Ok "+genre+" "+user+" je vous ouvre Youtube Music."
+                   }
+                   else
+                   {
+                       sortietext = "Voici Youtube Music "+user
+                   }
+                   web.open("https://music.youtube.com/")
+               }
+            }
         }
     }
     fun outNeuronText() :String
